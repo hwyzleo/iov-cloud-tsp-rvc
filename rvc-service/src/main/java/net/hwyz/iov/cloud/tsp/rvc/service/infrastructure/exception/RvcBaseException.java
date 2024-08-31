65a971aaba39f445dd7cc2cfbce9bba4d91805ef
hwyz_leo <hwyz.leo.ye@gmail.com>
2024-08-31 13:39:17 +0800

    功能: 增加全局异常处理

--- rvc-service/src/main/java/net/hwyz/iov/cloud/tsp/rvc/service/infrastructure/exception/RvcBaseException.java
+++ rvc-service/src/main/java/net/hwyz/iov/cloud/tsp/rvc/service/infrastructure/exception/RvcBaseException.java
@@ -1,31 +1,22 @@
 package net.hwyz.iov.cloud.tsp.rvc.service.infrastructure.exception;
 
+import net.hwyz.iov.cloud.tsp.framework.commons.exception.BaseException;
+
 /**
  * 远程车辆控制服务基础异常
  *
  * @author hwyz_leo
  */
-public class RvcBaseException extends RuntimeException {
+public class RvcBaseException extends BaseException {
 
     private static final int ERROR_CODE = 301000;
 
-    /**
-     * 错误码
-     */
-    private int code;
-    /**
-     * 错误消息
-     */
-    private String message;
-
     public RvcBaseException(String message) {
-        this.code = ERROR_CODE;
-        this.message = message;
+        super(ERROR_CODE, message);
     }
 
     public RvcBaseException(int errorCode, String message) {
-        this.code = errorCode;
-        this.message = message;
+        super(errorCode, message);
     }
 
 }
